23043a74b6355cf4cd1e8a74ccb6597f62b0dfda
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-07 15:25:38 +0200

    123

---
+++ lesson-1/02-fs/sum.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// alert(sum(1, 2)); // not working in Node.js
+// alert(sum(2, 2));
+
+console.log(sum(1, 2));
+console.log(sum(2, 2));
+console.log(sum(3, 4));
