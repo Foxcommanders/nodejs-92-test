59d4ceec60f6da9abba814fc2defa84d41f4392d
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-07 15:24:32 +0200

    123

--- lesson-1/02-fs/mix.js
+++ lesson-1/02-fs/mix.js
@@ -1,7 +1,7 @@
 const fs = require("node:fs/promises");
 
 async function main() {
-  const data = await fs.readFile("unknown.txt", { encoding: "utf-8" });
+  const data = await fs.readFile("mix.txt", { encoding: "utf-8" });
   const transformedDate = data.toUpperCase();
   await fs.writeFile("mix.txt", transformedDate);
 }
