bf66ca7c60ed8ce4381aacda04e0b9c7b33bc06d
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-09 15:56:21 +0200

    123

--- lesson-2/01-dirname/data/demo.js
+++ lesson-2/01-dirname/data/demo.js
@@ -1,8 +1,12 @@
 const fs = require("node:fs/promise");
 
 async function main() {
-    const data = await fs.readFile("movies.txt", { encoding: "utf-8"});
+  const data = await fs.readFile("movies.txt", { encoding: "utf-8" });
 
+  await fs.writeFile(
+    "movies.txt",
+    data.split("/n").filter((film) => film !== "Titanic")
+  );
 }
 
 main().catch(console.error);
