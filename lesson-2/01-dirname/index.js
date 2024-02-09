62603a42364de39f335bf4da83c88481b358b7f4
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-09 16:04:16 +0200

    123

--- lesson-2/01-dirname/index.js
+++ lesson-2/01-dirname/index.js
@@ -1,7 +1,6 @@
-const { error } = require("console");
 const movies = require("./movies");
 
 movies
   .readMovies()
   .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+  .catch((error) => console.error(error));
