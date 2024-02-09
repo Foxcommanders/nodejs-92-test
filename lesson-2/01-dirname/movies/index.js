b86e3fd55f9361a5284df1da2062cbbc1017a73b
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-09 16:01:02 +0200

    123

--- lesson-2/01-dirname/movies/index.js
+++ lesson-2/01-dirname/movies/index.js
@@ -2,8 +2,9 @@ const fs = require("node:fs/promise");
 const path = require("node:path");
 
 async function readMovies() {
-  const filePath = path.join;
-  const data = await fs.readFile("movies.txt");
+  // const filepath = path.join(__dirname, "../data/movies.txt");
+  const filePath = path.join(__dirname, "..", "data", "movies.txt");
+  const data = await fs.readFile(filePath, { encoding: "utf-8" });
 
   return data;
 }
