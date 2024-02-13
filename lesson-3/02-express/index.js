e367f16f5ec7d74e228091ad43be2a7b0b90cac0
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-13 20:19:35 +0200

    123

--- lesson-3/02-express/index.js
+++ lesson-3/02-express/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+console.log(req.method);
+console.log(res.url);
+
+res.end();
+});
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
+});
