4feda4ef53cb3f87ba95dd9f376add09b251f7e7
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-13 19:53:27 +0200

    123

--- lesson-3/01-http/index.js
+++ lesson-3/01-http/index.js
@@ -1,1 +1,12 @@
 const http = require("node:http");
+
+const server = http.createServer((request, response) => {
+    console.log({request});
+    console.log({response});
+
+    response.end();
+});
+
+server.listen(8080, () => {
+    console.log("Server started on port 8080");
+});
