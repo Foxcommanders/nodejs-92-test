fc8a06e378b490e3cd88e1ecb9c797ce02049c69
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-14 13:06:10 +0200

    123

--- lesson-3/04-cors/server.js
+++ lesson-3/04-cors/server.js
@@ -5,3 +5,10 @@ const cors = require("cors");
 const express = require("express");
 
 const app = express();
+
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+      optionsSuccessStatus: 200,
+    })
+  );
