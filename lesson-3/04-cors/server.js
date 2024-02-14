b27e3204a1aa5ed97eb701b0cac7a0e20fb06588
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-14 13:07:44 +0200

    123

--- lesson-3/04-cors/server.js
+++ lesson-3/04-cors/server.js
@@ -16,4 +16,13 @@ app.use(
   app.get("/books", async (req, res) => {
     const filePath = path.join(__dirname, "books.json");
 
-});
+    try {
+        const data = await fs.readFile(filePath, { encoding: "utf-8" });
+        const books = JSON.parse(data);
+    
+        res.send(books);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
