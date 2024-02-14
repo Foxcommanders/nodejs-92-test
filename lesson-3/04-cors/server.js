07421275d5ba2b3af2486870e1b5ceee0ba3f9f4
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-14 13:08:07 +0200

    123

--- lesson-3/04-cors/server.js
+++ lesson-3/04-cors/server.js
@@ -7,22 +7,22 @@ const express = require("express");
 const app = express();
 
 app.use(
-    cors({
-      origin: "http://localhost:3000",
-      optionsSuccessStatus: 200,
-    })
-  );
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
 
-  app.get("/books", async (req, res) => {
-    const filePath = path.join(__dirname, "books.json");
+app.get("/books", async (req, res) => {
+  const filePath = path.join(__dirname, "books.json");
 
-    try {
-        const data = await fs.readFile(filePath, { encoding: "utf-8" });
-        const books = JSON.parse(data);
-    
-        res.send(books);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-      }
-    });
+  try {
+    const data = await fs.readFile(filePath, { encoding: "utf-8" });
+    const books = JSON.parse(data);
+
+    res.send(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
