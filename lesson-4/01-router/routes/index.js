8c04c692867ea78b86c2544cc9acef9a4c908cad
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-15 20:14:12 +0200

    123

--- lesson-4/01-router/routes/index.js
+++ lesson-4/01-router/routes/index.js
@@ -4,3 +4,8 @@ const router = express.Router();
 
 const userRoutes = require('./users');
 const movieRoutes = require("./movies");
+
+router.use("/users", userRoutes);
+router.use("/movies", movieRoutes);
+
+module.exports = router;
