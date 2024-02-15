632770586fe0952ee96d132097e3bbb74f464619
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-15 20:04:36 +0200

    123

---
+++ lesson-4/01-router/routes/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/users", (req, res) => null);
+
+router.get("/users/1", (req, res) => null);
+
+router.post("/users", (req, res) => null);
+
+router.put("/users", (req, res) => null);
+
+router.delete("/users", (req, res) => null);
