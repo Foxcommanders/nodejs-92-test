725c90e925a95e48961d4e15a1f1f4a17a2e2bb5
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-15 20:24:05 +0200

    123

---
+++ lesson-4/02-params/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/api", routes);
+
+const router = express.Router();
+
+router.get("/users", (req, res) => null);
+
+router.get("/users/1", (req, res) => null);
+
+router.post("/users", (req, res) => null);
+
+router.put("/users", (req, res) => null);
+
+router.delete("/users", (req, res) => null);
+
+server.listen(8080, () => {
+    console.log("Server started on port 8080");
+  });
