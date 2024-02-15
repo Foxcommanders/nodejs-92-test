edb2559a9bb5dcd0bf083eea925306bb211c6572
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-15 20:10:33 +0200

    123

--- lesson-4/01-router/index.js
+++ lesson-4/01-router/index.js
@@ -2,16 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/users", (req, res) => null);
-
-app.get("/users/1", (req, res) => null);
-
-app.post("/users", (req, res) => null);
-
-app.put("/users", (req, res) => null);
-
-app.delete("/users", (req, res) => null);
-
 server.listen(8080, () => {
     console.log("Server started on port 8080");
   });
