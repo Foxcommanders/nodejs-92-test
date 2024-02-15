9327181cc5f3df7ee7b4794d543121d0fe02fd8c
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-15 20:27:01 +0200

    123

--- lesson-4/02-params/index.js
+++ lesson-4/02-params/index.js
@@ -2,19 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/api", routes);
+// app.use("/api", routes);
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/users", (req, res) => null);
+// router.get("/users", (req, res) => null);
 
-router.get("/users/1", (req, res) => null);
+// router.get("/users/1", (req, res) => null);
 
-router.post("/users", (req, res) => null);
+// router.post("/users", (req, res) => null);
 
-router.put("/users", (req, res) => null);
+// router.put("/users", (req, res) => null);
 
-router.delete("/users", (req, res) => null);
+// router.delete("/users", (req, res) => null);
+
+app.get("/movies/:id", (req, res) => {
+    console.log(req.params);
+
+    res.end();
+});
 
 server.listen(8080, () => {
     console.log("Server started on port 8080");
