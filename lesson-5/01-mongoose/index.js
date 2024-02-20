3815d83d2f1e664bae27d700bd8bf6ca18d5325a
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-02-20 21:36:21 +0200

    123

--- lesson-5/01-mongoose/index.js
+++ lesson-5/01-mongoose/index.js
@@ -1,3 +1,14 @@
 // import crypto from "node:crypto";
 
 // console.log(crypto.randomUUID);
+import mongoose from "mongoose";
+
+const DB_URI = "mongodb+srv://student:H7zUJYBZI9SzLPkW@cluster0.jj0vghs.mongodb.net/bookshelf?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log("Database connection success");
+    process.exit(0);
+  })
+  .cath((error) => console.error("Database connection error", error));
